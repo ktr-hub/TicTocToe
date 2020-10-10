0b31ad0b9c747504efb5b487456a9300f4d4da6e
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-10 14:50:58 +0530

    [Tirupathi Add. create Board]

---
+++ TicTocToe/TicTacToeGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TicTocToe
+{
+    class TicTacToeGame
+    {
+        public static char[] createBoard()
+        {
+            char []positions = new char[10];
+            for(int i = 1; i < 10; i++)
+            {
+                positions[i] = ' ';
+            }
+            return positions;
+        }
+        static void Main(string[] args)
+        {
+            char []positions = createBoard();
+        }
+    }
+}
